0f9ff88e033e6cd38a125eb8257a8b86b3ec2ce0
Felix Erkinger <felix@erkinger.at>
2016-09-25 13:21:28 +0200

    work on appliance start script

--- salt/appliance/start.sh
+++ salt/appliance/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # activate nginx, display "in startup"
-nginx_redirect_default starting.html
-if (get cert,self-sign if fail) display simple http&s page: Service not available
+nginx_redirect_to starting.html
 systemctl start nginx
 
 
@@ -13,7 +12,7 @@ if test $? -ne 0; then
     echo -n "error reading userdata: "
     printf "%s" "$userdata_yaml"| grep USERDATA_ERR
     # display error about missing userdata
-    nginx_redirect_default no-userdata.html
+    nginx_redirect_to no-userdata.html
     systemctl reload nginx
     exit 1
 fi
@@ -21,29 +20,18 @@ fi
 printf "%s" "$userdata_yaml" > /app/active-env.yml
 
 
-# update all packages
-nginx_redirect_default update-in-progress.html
-echo "fixme: update all packages"
-
-
-# letsencrypt setup
-domains=$(ENV_YML=/app/active-env.yml userdata_to_envlist ecs |
-    grep ECS_ALLOWED_HOSTS | sed -re "s/[^=]+=(.+)/\1/")
-domains_file=/usr/local/etc/dehydrated/domains.txt
-if test -e $domains_file; then rm $domains_file; fi
-for domain in $domains; do
-    printf "%s" "$domain" >> $domains_file
-done
-dehydrated -c
-
-
 # check if standby is true
 standby=$(ENV_YML=/app/active-env.yml userdata_to_envlist appliance |
     grep APPLIANCE_STANDBY | sed -re "s/[^=]+=(.+)/\1/" | tr A-Z a-z)
 if test "$standby" = "true"; then
-    nginx_redirect_default standby.html
+    nginx_redirect_to standby.html
     exit 1
 fi
+
+
+# update all packages
+nginx_redirect_to update-in-progress.html
+echo "fixme: update all packages"
 
 
 # storage setup
@@ -63,21 +51,32 @@ if test "$volatile_mount" = "" -a "$ignore_volatile" != "true" -o "$data_mount" 
     fi
     if test -z "$storage_setup"; then
         echo "error, empty or nonexisting ECS_STORAGE_SETUP, but storage is not ready"
-        nginx_redirect_default no-storage.html
+        nginx_redirect_to no-storage.html
         exit 1
     else
         salt-call state.sls appliance.storage
         err=$?
         if test "$err" -ne 0; then
             echo "error, appliance.storage_setup returned error: $err"
-            nginx_redirect_default no-storage.html
+            nginx_redirect_to no-storage.html
             exit 1
         fi
     fi
 fi
 
 
+# letsencrypt setup
+domains=$(ENV_YML=/app/active-env.yml userdata_to_envlist ecs |
+    grep ECS_ALLOWED_HOSTS | sed -re "s/[^=]+=(.+)/\1/")
+domains_file=/usr/local/etc/dehydrated/domains.txt
+if test -e $domains_file; then rm $domains_file; fi
+for domain in $domains; do
+    printf "%s" "$domain" >> $domains_file
+done
+dehydrated -c
 
+
+# postgres setup
 + look if postgres-data is found /data/postgres-ecs/*
 + start local postgres
 + no postgres-data or postgres-data but database is empty:
@@ -90,12 +89,8 @@ fi
     + not restored from dump ?
         + yes: create new database
 
-
+# start ecs
 + compose start ecs.* container
++ enable crontab entries (into container crontabs)
 + change nginx config, reload
-nginx_redirect_default --disable
-
-
-
-### start errors
-    + recover_from_backup but error while duplicity restore/connect ("recover from backup error")
+nginx_redirect_to --disable
