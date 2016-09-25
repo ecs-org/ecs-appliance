4d66ec2699af8666945d5f8203ddd3e16c14f077
Felix Erkinger <felix@erkinger.at>
2016-09-25 22:10:05 +0200

    docker-compose: export ports to localhost: 5000 for ecs, 5090 for mocca, 5095 for pdfas, 5080 for cadvisor start.sh: work on ssl and dh params, cleanup nginx.conf:ssl config, proxy_config status.html: minor display cleanup

--- salt/appliance/start.sh
+++ salt/appliance/start.sh
@@ -11,19 +11,18 @@ userdata_yaml=$(get_userdata)
 if test $? -ne 0; then
     echo -n "error reading userdata: "
     printf "%s" "$userdata_yaml"| grep USERDATA_ERR
-    # display error about missing userdata
     nginx_redirect_to no-userdata.html
     systemctl reload nginx
     exit 1
 fi
 # write to /app/active-env.yml
 printf "%s" "$userdata_yaml" > /app/active-env.yml
+# activate all yaml into environment
+ENV_YML=/app/active-env.yml update_env_from_userdata
 
 
 # check if standby is true
-standby=$(ENV_YML=/app/active-env.yml userdata_to_envlist appliance |
-    grep APPLIANCE_STANDBY | sed -re "s/[^=]+=(.+)/\1/" | tr A-Z a-z)
-if test "$standby" = "true"; then
+if test "$($APPLIANCE_STANDBY| tr A-Z a-z)" = "true"; then
     nginx_redirect_to standby.html
     exit 1
 fi
@@ -35,18 +34,16 @@ echo "fixme: update all packages"
 
 
 # storage setup
-storage=$(ENV_YML=/app/active-env.yml userdata_to_envlist appliance | grep APPLIANCE_STORAGE)
-ignore_volatile=$(printf "%s" $storage | grep _IGNORE_VOLATILE | sed -re "s/[^=]+=(.+)/\1/" | tr A-Z a-z)
-ignore_data=$(printf "%s" $storage | grep _IGNORE_DATA | sed -re "s/[^=]+=(.+)/\1/" | tr A-Z a-z)
-storage_setup=$(printf "%s" $storage | grep STORAGE_SETUP | sed -re "s/[^=]+=(.+)/\1/")
 volatile_mount=$(findmnt -S "LABEL=ecs-volatile" -f -l -n -o "TARGET")
 data_mount=$(findmnt -S "LABEL=ecs-data" -f -l -n -o "TARGET")
+ignore_volatile="$($APPLIANCE_STORAGE_IGNORE_VOLATILE | tr A-Z a-z)"
+ignore_data="$($APPLIANCE_STORAGE_IGNORE_DATA | tr A-Z a-z)"
 
-if test "$volatile_mount" = "" -a "$ignore_volatile" != "true" -o "$data_mount" = "" -a "$ignore_data" != "true"; then
-    if test "$volatile_mount" = "" -a "$ignore_volatile" != "true"; then
+if test "$volatile_mount" = "" -a  "$ignore_volatile" != "true" -o "$data_mount" = "" -a "$ignore_data" != "true"; then
+    if test "$volatile_mount" = "" -a "$ignore_volatile" != "true" then
         echo "error finding mount for ecs-volatile filesystem"
     fi
-    if test "$data_mount" = "" -a "$ignore_data" != "true"; then
+    if test "$data_mount" = "" -a "$ignore_data" != "true" then
         echo "error finding mount for ecs-data filesystem"
     fi
     if test -z "$storage_setup"; then
@@ -54,10 +51,11 @@ if test "$volatile_mount" = "" -a "$ignore_volatile" != "true" -o "$data_mount" 
         nginx_redirect_to no-storage.html
         exit 1
     else
+        echo "calling appliance.storage setup"
         salt-call state.sls appliance.storage
         err=$?
         if test "$err" -ne 0; then
-            echo "error, appliance.storage_setup returned error: $err"
+            echo "error, appliance.storage setup returned error: $err"
             nginx_redirect_to no-storage.html
             exit 1
         fi
@@ -65,15 +63,20 @@ if test "$volatile_mount" = "" -a "$ignore_volatile" != "true" -o "$data_mount" 
 fi
 
 
-# letsencrypt setup
-domains=$(ENV_YML=/app/active-env.yml userdata_to_envlist ecs |
-    grep ECS_ALLOWED_HOSTS | sed -re "s/[^=]+=(.+)/\1/")
+# generate certificates using letsencrypt (dehydrated client)
 domains_file=/usr/local/etc/dehydrated/domains.txt
 if test -e $domains_file; then rm $domains_file; fi
-for domain in $domains; do
+for domain in $ECS_ALLOWED_HOSTS; do
     printf "%s" "$domain" >> $domains_file
 done
 dehydrated -c
+
+# re-generate dhparam.pem if not found or less than 2048 bit
+if test ! -e /etc/nginx/certs/dhparam.pem -o "$(stat -L -c %s /etc/nginx/certs/dhparam.pem)" -lt 224; then
+    mkdir -p /etc/nginx/certs
+    echo "no, or to small dh.param found, regenerating with 2048 bit (takes a few minutes)"
+    openssl dhparam 2048 -out /etc/nginx/certs/dhparam.pem
+fi
 
 
 # postgres setup
@@ -88,6 +91,7 @@ dehydrated -c
         + premigrate (if old dump) and migrate
     + not restored from dump ?
         + yes: create new database
+
 
 # start ecs
 + compose start ecs.* container
