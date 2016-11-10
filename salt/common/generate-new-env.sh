1382d7145b286dcd7f73ab1f9b606c783fab2982
Felix Erkinger <felix@erkinger.at>
2016-11-10 03:15:10 +0100

    env generation

--- salt/common/generate-new-env.sh
+++ salt/common/generate-new-env.sh
@@ -4,12 +4,15 @@ realpath=`dirname $(readlink -e "$0")`
 
 if test -z "$2"; then
     cat << EOF
-Usage: "$0 domain targetdir"
+Usage: "$0 domain targetdir [optional parameter for salt-call]"
 
 EOF
     exit 1
 fi
 
-targetdir=$(readlink -e "$2")
-
-sudo salt-call state.sls common.env_gen pillar="{\"domain\": \"$1\", \"targetdir\": \"$targetdir\"}"
+domain=$1
+targetdir=$(readlink -f "$2")
+shift 2
+sudo mkdir -p $targetdir
+sudo salt-call state.sls common.env_gen pillar="{\"domain\": \"$domain\", \"targetdir\": \"$targetdir\"}" $@
+sudo chown $USER:$USER $targetdir/env.yml
