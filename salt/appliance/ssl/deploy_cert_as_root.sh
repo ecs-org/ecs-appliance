3f8ea326ec0e7f73670c2aef00c6e9d3853668ab
Felix Erkinger <felix@erkinger.at>
2016-11-15 01:07:35 +0100

    cert cleanup

--- salt/appliance/ssl/deploy_cert_as_root.sh
+++ salt/appliance/ssl/deploy_cert_as_root.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/local/sbin/unchanged_cert_as_root.sh $1 $2 $3 $4 $5
+/usr/local/sbin/unchanged_cert_as_root.sh "$@"
 systemctl reload-or-restart nginx
 systemctl restart stunnel
 systemctl restart postfix
