1a401f8b22bb9ba6b5ccf723fe185f51d3ede07e
Felix Erkinger <felix@erkinger.at>
2016-11-19 17:42:08 +0100

    workaround systemd limitation in EnvironmentFile (no usable multiline support)

--- salt/appliance/scripts/prepare-env.sh
+++ salt/appliance/scripts/prepare-env.sh
@@ -14,12 +14,9 @@ printf '%s' "$userdata_yaml" | grep USERDATA_TYPE
 echo "write userdata to /app/active-env.yml"
 printf "%s" "$userdata_yaml" > /app/active-env.yml
 chmod 0600 /app/active-env.yml
-echo "write systemd EnvironmentFile to /app/active-env.env"
-ENV_YML=/app/active-env.yml userdata_to_envlist ecs,appliance --alt-multiline > /app/active-env.env
-chmod 0600 /app/active-env.env
 
 # test: export active yaml into environment
-ENV_YML=/app/active-env.yml update_env_from_userdata ecs,appliance
+ENV_YML=/app/active-env.yml userdata_to_env ecs,appliance
 if test $? -ne 0; then
     appliance_exit "Appliance Error" "Could not activate userdata environment"
 fi
