19e27701c9af89e056b451d0f203f3bc7e242840
Felix Erkinger <felix@erkinger.at>
2016-12-12 21:59:22 +0100

    run dehydrated as root, comment out restart appliance in hook script

--- salt/appliance/ssl/deploy-cert-as-root.sh
+++ salt/appliance/ssl/deploy-cert-as-root.sh
@@ -3,4 +3,6 @@
 systemctl reload-or-restart nginx
 systemctl restart stunnel
 systemctl restart postfix
-systemctl restart appliance
+# systemctl restart appliance
+# fixme: must be done as signal but not interupting
+# only needed for pdfas and mocca
