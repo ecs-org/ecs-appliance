56a29780bca8b0e70099e99eaf0a12ed82edb3a9
Felix Erkinger <felix@erkinger.at>
2017-01-29 01:22:23 +0100

    update qrcode

--- salt/qrcode/data2qrpdf.sh
+++ salt/qrcode/data2qrpdf.sh
@@ -2,25 +2,37 @@
 
 usage() {
     cat <<EOF
-Usage: $0 datafile
+Usage: $0 [--chunksize bytes] datafile
 
 input (compressed) binary data,
 encode base32 and generate one or more alphanumeric qrcodes,
 arrange them in a 2x2 matrix per page pdf and output to \${datafile}.pdf
 
-Limits:
- * Single QRCode:
-   * Medium Error Correction: Max Version 40:
-     * 3391 alphanumeric (3376) => base32 <= 2110 Bytes (8 Bit)
++ Single QRCode, Medium Error Correction, Version 40
+    + 3391 alphanumeric (3378) => base32 <= 2111 Bytes (8 Bit)
 
- * manually linked QRCode, Version 29, 4 Codes per A4 Page (25 A4 Pages Maximum)
-   * Medium Error Correction: 100 x Version 29:
-     * 1839 alphanumeric (base32 decode of (1839 -6 (padding) -3 (split header) -4 (safety)= 1826):
-       * 1141 * 100 ~<= 114.100 Bytes (8 Bit)
++ linked QRCode, Medium Error Correction, Version 29
+    + 4 Codes per A4 Page (25 A4 Pages Maximum) maximum of 100 Codes
+    + 1 Code = Total chars -6 padding -3 split header -4 safety = available chars
+    + 100 Codes= 1826 chars <= base32 = 1141 Bytes * 100 ~<= 114.100 Bytes (8 Bit)
 
-QR-Code Standard:
- * Version 40: alphanumeric Limits: L 4296, M 3391, Q 2420, H 1852
- * Version 29: alphanumeric Limits: L 2369, M 1839, Q 1322, H 1016
+QR-Code (type alphanumeric) Dimensions:
+
+Minimum Module Size on 300dpi(12dot/mm): 0.33mm
+    + Double the Size for 150dpi(6dot/mm): 0.66mm
+
+| Ver | Modules | min mm | Limits                         | char | bytes
+| --- | ------- | ------ | ------------------------------ | ---- | -----
+| 40  | 177x177 | 58,41  | L 4296, M 3391, Q 2420, H 1852 | 3378 | 2111
+| 34  | 153x153 | 50,49  | L 3183, M 2506, Q 1787, H 1394 | 2493 | 1558
+| 33  | 149x149 | 49,17  | L 3009, M 2369, Q 1700, H 1307 | 2356 | 1472
+| 32  | 145x145 | 47,85  | L 2840, M 2238, Q 1618, H 1226 | 2225 | 1390
+| 31  | 141x141 | 46,53  | L 2677, M 2113, Q 1499, H 1150 | 2100 | 1312
+| 30  | 137x137 | 45,21  | L 2520, M 1994, Q 1429, H 1080 | 1981 | 1238
+| 29  | 133x133 | 43,89  | L 2369, M 1839, Q 1322, H 1016 | 1826 | 1141
+| 28  | 129x129 | 42,57  | L 2223, M 1732, Q 1263, H  958 | 1719 | 1074
+| 27  | 125x125 | 41,25  | L 2132, M 1637, Q 1172, H  910 | 1624 | 1015
+
 
 Tests:
  * $0 --unittest
@@ -65,30 +77,42 @@ data2pdf() {
     local fname=`readlink -f $1`
     local fbase=`basename $fname`
     local fsize=`stat -c "%s" $fname`
+    local maxsingle=2111
+    local chunksize=1390
+    local maxchained=${chunksize}00
+    local level="M"
 
     if test ! -f $fname; then
         echo "ERROR: could not find datafile $fname; call $0 for usage information"
         exit 2
     fi
 
-    if test $fsize -gt 114100; then
-        echo "ERROR: source file bigger than max capacity of 1141*100 bytes ($fsize); call $0 for usage information"
+    if test $fsize -gt $maxchained; then
+        echo "ERROR: source file bigger than max capacity of $maxchained ($fsize); call $0 for usage information"
         exit 3
     fi
 
-    local tempdir=`mktemp -d`
+    local tempdir=$(mktemp -d)
     if test ! -d $tempdir; then echo "ERROR: creating tempdir"; exit 10; fi
 
-    if test $fsize -le 2110; then
-        cat $fname | python -c "import sys, base64; sys.stdout.write(base64.b32encode(sys.stdin.read()))" | qrencode -o $tempdir/$fbase.png -l M -i
+    if test $fsize -le $maxsingle; then
+        cat $fname | python -c "import sys, base64; \
+            sys.stdout.write(base64.b32encode(sys.stdin.read()))" | \
+            qrencode -l $level -i -o $tempdir/$fbase.png
         montage -label '%f' -page A4 -geometry +10 $tempdir/$fbase.png ${fbase}.pdf
     else
-        cat $fname | python -c "import sys, base64; sys.stdout.write(base64.b32encode(sys.stdin.read()))" | split -a 2 -b 1826 -d - $tempdir/$fbase-
-        for a in `ls $tempdir/$fbase-* | sort -n`; do
-            echo -n "${a: -2:2} " | cat - $a | qrencode -o $tempdir/`basename $a`.png -l M -i
+        cat $fname | python -c "import sys, base64; \
+            sys.stdout.write(base64.b32encode(sys.stdin.read()))" | \
+            split -a 2 -b $chunksize -d - "$tempdir/$fbase-"
+        for a in $(ls $tempdir/$fbase-* | sort -n); do
+            echo -n "${a: -2:2} " | cat - $a | \
+                qrencode -l $level -i -t EPS -o "$tempdir/$(basename $a).eps"
         done
-        list=`ls $tempdir/$fbase*.png | sort -n | tr "\n" " "`
-        montage -label '%f' -page A4 -tile 2x2 -geometry +10 $list ${fbase}.pdf
+        list=$(ls $tempdir/$fbase*.eps | sort -n | tr "\n" " ")
+        montage -label '%f' -tile 2x2 -geometry +10 \
+            $list "$tempdir/page-${fbase}-%d.eps"
+        list=$(ls $tempdir/page-$base*.eps | sort -n | tr "\n" " ")
+        convert -gravity east -format pdf $list ${fbase}.pdf
     fi
 
     if test -d $tempdir; then
--- salt/qrcode/qrpdf2data.sh
+++ salt/qrcode/qrpdf2data.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 usage() {
-    cat <<EOF
+    cat << EOF
 Usage: $0 {pdffile|picture [picture]*} outputfile
 
 example:
   $0 test.pdf test.zip
   $0 *.png env.yml.gz; gzip -d env.yml.gz
 
-input qrcode pictures or a pdf with qrcodes.
+input linked qrcode pictures or a pdf with linked qrcodes.
 decode the qrcodes and base32 decode the resulting output.
 input order of pictures is not relevant for the output.
 
@@ -19,7 +19,7 @@ EOF
 if test "$2" = ""; then usage; fi
 if test ! -f $1; then usage; fi
 
-if ! `which zbarimg`; then
+if ! which zbarimg > /dev/null; then
     echo "error, no zbarimg found. try 'apt install zbar-tools'"
     exit 1
 fi
