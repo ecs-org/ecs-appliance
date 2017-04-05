cc5fc27dfcb26fd01835f2f3b6e4728ef32bbdc2
Felix Erkinger <felix@erkinger.at>
2017-04-05 13:16:57 +0200

    add opendkim support to appliance.  - update env-template.yml to include DKIM key generation  - changed postfix config to use opendkim as milter

--- salt/appliance/postfix/init.sls
+++ salt/appliance/postfix/init.sls
@@ -16,6 +16,46 @@ include:
         additional_net: {{ salt['pillar.get']('docker:net') }}
         domain: {{ salt['pillar.get']('appliance:domain') }}
 
+/etc/opendkim.conf:
+  file.managed:
+    - source: salt://appliance/postfix/opendkim.conf
+    - template: jinja
+    - defaults:
+        additional_net: {{ salt['pillar.get']('docker:net') }}
+        domain: {{ salt['pillar.get']('appliance:domain') }}
+
+/etc/default/opendkim:
+  file.managed:
+    - source: salt://appliance/postfix/opendkim.default
+
+opendkim:
+  pkg.installed:
+    - pkgs:
+      - opendkim
+      - opendkim-tools
+
+{%- set dkimkey= salt['pillar.get']('appliance:dkim:key', False) or salt['cmd.run_stdout']('openssl genrsa 2048') %}
+
+/etc/dkimkeys/dkim.key:
+  file.managed:
+    - user: opendkim
+    - group: opendkim
+    - mode: "0600"
+    - makedirs: true
+    - contents: |
+{{ dkimkey|indent(8,True) }}
+
+opendkim.service:
+  service.running:
+    - name: opendkim
+    - enable: true
+    - require:
+      - pkg: opendkim
+      - file: /etc/dkimkeys/dkim.key
+    - watch:
+      - file: /etc/dkimkeys/dkim.key
+      - file: /etc/default/opendkim
+
 postfix:
   pkg.installed:
     - pkgs:
@@ -27,6 +67,7 @@ postfix:
     - enable: true
     - require:
       - pkg: postfix
+      - service: opendkim.service
       - sls: docker
     - watch:
       - file: /etc/postfix/main.cf
--- salt/appliance/postfix/prepare-postfix.sh
+++ salt/appliance/postfix/prepare-postfix.sh
@@ -1,8 +1,29 @@
 prepare_postfix () {
+    local restart_opendkim=false
+    # ### opendkim: rewrite opendkim.conf with APPPLIANCE_DOMAIN
+    sed -i.bak  "s/^Domain.*/Domain                 $APPLIANCE_DOMAIN/" /etc/opendkim.conf
+    if ! diff -q /etc/opendkim.conf.bak /etc/opendkim.conf; then
+        echo "opendkim.conf changed"
+        diff -u /etc/opendkim.conf.bak /etc/opendkim.conf
+        restart_opendkim=true
+    fi
+    # ### opendkim: rewrite rsa key if not existing or different with APPLIANCE_DKIM_KEY
+    if test ! -d /etc/dkimkeys; then mkdir /etc/dkimkeys; fi
+    echo "$APPLIANCE_DKIM_KEY" > /etc/dkimkeys/dkim.key.new
+    chown opendkim /etc/dkimkeys/dkim.key.new
+    chmod "0600" /etc/dkimkeys/dkim.key.new
+    if ! diff -q /etc/dkimkeys/dkim.key /etc/dkimkeys/dkim.key.new; then
+        echo "/etc/dkimkeys/dkim.key changed"
+        mv /etc/dkimkeys/dkim.key.new /etc/dkimkeys/dkim.key
+        restart_opendkim=true
+    fi
+    if $restart_opendkim; then
+        systemctl restart opendkim
+    fi
     # ### postfix: rewrite postfix main.cf with APPLIANCE_DOMAIN, restart postfix (ssl keys change)
     sed -i.bak  "s/^myhostname.*/myhostname = $APPLIANCE_DOMAIN/;s/^mydomain.*/mydomain = $APPLIANCE_DOMAIN/" /etc/postfix/main.cf
     if ! diff -q /etc/postfix/main.cf /etc/postfix/main.cf.bak; then
-        echo "postfix configuration changed"
+        echo "postfix main.cf changed"
         diff -u /etc/postfix/main.cf.bak /etc/postfix/main.cf
     fi
     systemctl restart postfix
