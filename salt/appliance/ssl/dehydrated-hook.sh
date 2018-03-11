d55fa3fdac1254c70ed163e11f5452f9d4ff2909
Felix Erkinger <felix@erkinger.at>
2018-03-11 14:04:31 +0100

    dehydrated updates:
      - update dehydrated to master at 6e802dd , to include 7a0e71c (follow location on http get-requests)
      - update dehydrated-hook.sh to add support for unknown hooks

--- salt/appliance/ssl/dehydrated-hook.sh
+++ salt/appliance/ssl/dehydrated-hook.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 deploy_challenge() {
 local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 }
@@ -7,15 +8,41 @@ clean_challenge() {
 local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 }
 
-unchanged_cert() {
-local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
-sudo /usr/local/sbin/unchanged-cert-as-root.sh $1 $2 $3 $4 $5
-}
-
 deploy_cert() {
 local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
 sudo /usr/local/sbin/deploy-cert-as-root.sh $1 $2 $3 $4 $5 $6
 }
 
+unchanged_cert() {
+local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+sudo /usr/local/sbin/unchanged-cert-as-root.sh $1 $2 $3 $4 $5
+}
+
+invalid_challenge() {
+    local DOMAIN="${1}" RESPONSE="${2}"
+}
+
+request_failure() {
+    local STATUSCODE="${1}" REASON="${2}" REQTYPE="${3}" HEADERS="${4}"
+}
+
+generate_csr() {
+    local DOMAIN="${1}" CERTDIR="${2}" ALTNAMES="${3}"
+}
+
+startup_hook() {
+  # This hook is called before the cron command to do some initial tasks
+  # (e.g. starting a webserver).
+  :
+}
+
+exit_hook() {
+  # This hook is called at the end of the cron command and can be used to
+  # do some final (cleanup or other) tasks.
+  :
+}
+
 HANDLER="$1"; shift
-"$HANDLER" "$@"
+if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge|deploy_cert|unchanged_cert|invalid_challenge|request_failure|generate_csr|startup_hook|exit_hook)$ ]]; then
+  "$HANDLER" "$@"
+fi
